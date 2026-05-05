b9adbae2be3a5557054551d17157b70ad780ff41
GOMES Mathéo <matheo.gomes@externe.maif.fr>
2026-05-05 12:31:23 +0200

    fixing wrong output (show &{email} instead of the email)

--- index.js
+++ index.js
@@ -3,9 +3,9 @@ import validator from 'validator';
 
 function validateEmail(email) {
 	if(validator.isEmail(email)) {
-		return console.log(chalk.green("${email} est valide"));
+		return console.log(chalk.green(`${email} est valide`));
 	} else {
-		console.log(chalk.red("${email} est mal formaté"));
+		console.log(chalk.red(`${email} est mal formaté`));
 	}
 }
 
